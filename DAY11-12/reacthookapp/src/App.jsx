1036de54becb7cd1288bb3e6f84a8172cfd54c45
정민영 <myjeong19@naver.com>
2024-02-23 10:53:47 +0900

    useMemo

--- DAY11-12/reacthookapp/src/App.jsx
+++ DAY11-12/reacthookapp/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css';
-import { UseEffectHook } from './hooks/UseEffectHook';
-import { UseStateHook } from './hooks/UseStateHook';
-import { UserReducerHook } from './hooks/UserReducerHook';
+// import { UseStateHook } from './hooks/UseStateHook';
+// import { UserReducerHook } from './hooks/UserReducerHook';
+// import { UseEffectHook } from './hooks/UseEffectHook';
+import { UseMemo } from './hooks/UseMemo';
 
 function App() {
   return (
     <div className='App'>
-      <UseStateHook />
+      {/* <UseStateHook />
       <hr />
       <UserReducerHook />
       <hr />
       <UseEffectHook />
+      <hr /> */}
+      <UseMemo />
     </div>
   );
 }
---
+++ DAY11-12/reacthookapp/src/hooks/UseMemo.jsx
@@ -0,0 +1,44 @@
+import { useState, useMemo } from 'react';
+
+const getAverage = numbers => {
+  if (numbers.length === 0) return 0;
+
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
+};
+
+export const UseMemo = () => {
+  const [number, setNumber] = useState(0);
+  const [list, setList] = useState([]);
+
+  const handleNumber = () => setNumber(prevNumber => prevNumber + 1);
+
+  const handleAdd = () => {
+    setList(prevList => {
+      const updatedList = prevList.concat(+number);
+      setNumber(0);
+      return updatedList;
+    });
+  };
+
+  const average = useMemo(() => getAverage(list), [list]);
+
+  //   const average = getAverage(list);
+
+  return (
+    <>
+      <input type='text' value={number} onChange={handleNumber} />
+      <button onClick={handleAdd}>ADD</button>
+
+      <div>
+        <b>평균 값: {average}</b>
+      </div>
+
+      <ul>
+        {list.map((number, index) => {
+          return <li key={index}>{number}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
